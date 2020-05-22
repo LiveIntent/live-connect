7a2c05d145521aee32f88f941c6b2e8897b5c723
janko <janko.ulaga@gmail.com>
2020-05-22 14:45:41 +0200

    RTA-93. Spec fix to use the new function.

--- test/unit/enricher/legacy-duid.spec.js
+++ test/unit/enricher/legacy-duid.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { resolve } from '../../../src/enrichers/legacy-duid'
+import { enrich } from '../../../src/enrichers/legacy-duid'
 import jsdom from 'mocha-jsdom'
 import * as storage from '../../../src/utils/storage'
 
@@ -11,7 +11,7 @@ describe('LegacyDuidEnricher', () => {
 
   it('should read the legacyId', function () {
     storage.setDataInLocalStorage('_litra_id.edec', 'a-0z68--e4f71227-70d0-4e54-b1c3-ACf80616bbb0.1538047703.39.1573030646.1550763182.eaff6045-fa6f-4073-9397-598a9e64ca12')
-    const resolutionResult = resolve({ appId: 'a-XXXX' }, storage)
+    const resolutionResult = enrich({ appId: 'a-XXXX' }, storage)
     expect('a-0z68--e4f71227-70d0-4e54-b1c3-ACf80616bbb0').to.eql(resolutionResult.legacyId.duid)
   })
 })
