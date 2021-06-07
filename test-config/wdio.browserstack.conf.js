fbbe6a3d959e7f9362c38d32e379ea3ba1464f90
Paulo Baena <jpbaena@liveintent.com>
2021-06-07 10:25:09 +0200

    CM-75: update timeout for tests

--- test-config/wdio.browserstack.conf.js
+++ test-config/wdio.browserstack.conf.js
@@ -118,7 +118,7 @@ exports.config = {
   baseUrl: 'http://localhost',
   //
   // Default timeout for all waitFor* commands.
-  waitforTimeout: 60000,
+  waitforTimeout: 90000,
   //
   // Default timeout in milliseconds for request
   // if Selenium Grid doesn't send response
@@ -167,7 +167,7 @@ exports.config = {
   // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: 'bdd',
-    timeout: 60000,
+    timeout: 90000,
     require: ['@babel/register']
   }
 
