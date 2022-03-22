6b82ac5c130b4fb22abf55391b44643679c74475
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2022-03-22 19:06:37 +0100

    don't pin selenium

--- test-config/wdio.browserstack.conf.js
+++ test-config/wdio.browserstack.conf.js
@@ -6,8 +6,7 @@ const commonCapabilities = {
   'browserstack.console': 'verbose',
   'browserstack.debug': true,
   'browserstack.networkLogs': true,
-  'browserstack.appium_version': '1.14.0',
-  'browserstack.selenium_version': '3.14.0'
+  'browserstack.appium_version': '1.14.0'
 }
 
 // https://www.browserstack.com/automate/capabilities
