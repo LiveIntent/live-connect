87c51ec14c960084024e22d2a0e3eb6c6a951791
wiem <welabidine@liveintent.com>
2022-04-13 16:23:43 +0200

    add gdprApplies in preemble for it test

--- test/it/helpers/preambled.js
+++ test/it/helpers/preambled.js
@@ -6,7 +6,7 @@ const helpers = require('../../../src/utils/types')
 const customerSpecifics = window.LI || {}
 const queue = window.liQ || []
 
-const lc = liveConnect.StandardLiveConnect(helpers.merge(customerSpecifics, { trackerName: 'LC_VERSION', contextSelectors: 'p', contextElementsLength: '100' }), storage, calls)
+const lc = liveConnect.StandardLiveConnect(helpers.merge(customerSpecifics, { trackerName: 'LC_VERSION', contextSelectors: 'p', contextElementsLength: '100', gdprApplies: true }), storage, calls)
 
 if (Array.isArray(queue)) {
   for (let i = 0; i < queue.length; i++) {
