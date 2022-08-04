794ec33d37aa334405eb988fdd2d180ad1fa80a2
Viktor Dreiling <viktor@Viktors-MacBook-Pro.local>
2022-08-04 15:50:44 +0200

    Remove LC_SHUTDOWN constant

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -2,7 +2,6 @@ export const EVENT_BUS_NAMESPACE = '__li__evt_bus'
 export const ERRORS_PREFIX = 'li_errors'
 export const PIXEL_SENT_PREFIX = 'lips'
 export const PRELOAD_PIXEL = 'pre_lips'
-export const LC_SHUTDOWN = 'LC_SHUTDOWN'
 
 export const PEOPLE_VERIFIED_LS_ENTRY = '_li_duid'
 
