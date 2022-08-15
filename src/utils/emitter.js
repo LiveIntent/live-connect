f62c8b0a23d18ef75972cb89821f38d719922fef
Viktor Dreiling <viktor@Viktors-MBP.fritz.box>
2022-08-15 10:54:36 +0200

    Remove obsolete Emitter

--- src/utils/emitter.js
+++ src/utils/emitter.js
@@ -1,39 +1,5 @@
 import * as C from '../utils/consts'
 
-export function Emitter (globalBus, localBus) {
-  function _emit (prefix, message) {
-    globalBus && globalBus.emit(prefix, message)
-    localBus && localBus.emit(prefix, message)
-  }
-
-  function _fromError (name, exception) {
-    _error(name, exception.message, exception)
-  }
-
-  function _error (name, message, e = {}) {
-    const wrapped = new Error(message || e.message)
-    wrapped.stack = e.stack
-    wrapped.name = name || 'unknown error'
-    wrapped.lineNumber = e.lineNumber
-    wrapped.columnNumber = e.columnNumber
-    _emit(C.ERRORS_PREFIX, wrapped)
-  }
-
-  return {
-    emit: _emit,
-    fromError: _fromError,
-    error: _emit
-  }
-}
-
-export function NoOpEmitter () {
-  return {
-    emit: {},
-    fromError: {},
-    error: {}
-  }
-}
-
 function _emit (prefix, message) {
   window && window[C.EVENT_BUS_NAMESPACE] && window[C.EVENT_BUS_NAMESPACE].emit(prefix, message)
 }
