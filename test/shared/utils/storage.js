680be984a1692eac25bd8d9d4658b5784d6035e3
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2022-08-18 13:55:20 +0200

    [CM-565] Use latest code from liveconnectbuilder for tests

--- test/shared/utils/storage.js
+++ test/shared/utils/storage.js
@@ -7,10 +7,21 @@
  * @property {(boolean|undefined)} [httponly]
  * @property {((''|'Strict'|'Lax')|undefined)} [samesite]
  */
-import * as cookies from 'browser-cookies'
-import * as emitter from '../../../src/utils/emitter'
+import Cookies from 'js-cookie'
+import * as emitter from 'live-connect-js/src/utils/emitter'
 
 let _localStorageIsEnabled = null
+
+const cookies = Cookies.withConverter({
+  read: function (value, name) {
+    try {
+      return Cookies.converter.read(value, name)
+    } catch (e) {
+      emitter.error('CookieReadError', `Failed reading cookie ${name}`, e)
+      return undefined // default implementation will return undefined if cookie is not found
+    }
+  }
+})
 
 /**
  * @returns {boolean}
@@ -29,9 +40,9 @@ export function localStorageIsEnabled () {
  */
 function _checkLocalStorage () {
   let enabled = false
-  const key = '__live-connect-localstorage-probe'
   try {
     if (window && window.localStorage) {
+      const key = Math.random().toString()
       window.localStorage.setItem(key, key)
       enabled = window.localStorage.getItem(key) === key
       window.localStorage.removeItem(key)
@@ -76,24 +87,19 @@ export function getDataFromLocalStorage (key) {
  * @return {[String]}
  */
 export function findSimilarCookies (keyLike) {
-  const ret = []
   try {
-    const allCookies = cookies.all()
-    for (const cookieName in allCookies) {
-      if (allCookies[cookieName] && cookieName.indexOf(keyLike) >= 0) {
-        ret.push(cookies.get(cookieName))
-      }
-    }
+    const allCookies = cookies.get()
+    return Object.keys(allCookies).filter(key => key.indexOf(keyLike) >= 0 && allCookies[key] !== undefined).map(key => allCookies[key])
   } catch (e) {
     emitter.error('CookieFindSimilarInJar', 'Failed fetching from a cookie jar', e)
+    return []
   }
-  return ret
 }
 
 /**
  * @param {string} key
  * @param {string} value
- * @param {number} expires
+ * @param {string} expires
  * @param {string} sameSite
  * @param {string} domain
  * @returns void
@@ -109,16 +115,5 @@ export function setCookie (key, value, expires, sameSite, domain) {
 export function removeDataFromLocalStorage (key) {
   if (localStorageIsEnabled()) {
     window.localStorage.removeItem(key)
-  }
-}
-
-/**
- * @param {string} key
- * @param {string} value
- * @returns {string|null}
- */
-export function setDataInLocalStorage (key, value) {
-  if (localStorageIsEnabled()) {
-    window.localStorage.setItem(key, value)
   }
 }
