e9d9acbc3d0894809ac0857eb8d3023f04d97522
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2022-09-21 13:45:02 +0200

    typescript wip

--- rollup-modules.config.js
+++ rollup-modules.config.js
@@ -4,29 +4,31 @@ import commonjs from 'rollup-plugin-commonjs'
 import strip from '@rollup/plugin-strip'
 import babel from 'rollup-plugin-babel'
 import cleanup from 'rollup-plugin-cleanup'
+import typescript from '@rollup/plugin-typescript'
 
 const OUTPUT_DIR = './'
 const plugins = [
   resolve(),
+  typescript(),
   commonjs(),
   babel(),
   cleanup(),
   strip()
 ]
 
-const filenames = ['initializer.js', 'standard-live-connect.js', 'minimal-live-connect.js']
+const filenames = ['initializer.ts', 'standard-live-connect.js', 'minimal-live-connect.js']
 const outputs = filenames.reduce((accumulator, sourceFile) => {
   const action = [{
     input: `src/${sourceFile}`,
     output: {
-      file: `${OUTPUT_DIR}esm/${sourceFile}`,
+      file: `${OUTPUT_DIR}esm/${sourceFile.replace(".ts", ".js")}`,
       format: 'esm'
     },
     plugins: plugins
   }, {
     input: `src/${sourceFile}`,
     output: {
-      file: `${OUTPUT_DIR}cjs/${sourceFile}`,
+      file: `${OUTPUT_DIR}cjs/${sourceFile.replace(".ts", ".js")}`,
       format: 'cjs'
     },
     plugins: plugins
