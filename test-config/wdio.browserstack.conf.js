3bf8ca2770a958f5cec306df4507373061641f31
wiem <welabidine@liveintent.com>
2022-10-25 15:42:19 +0200

    fix

--- test-config/wdio.browserstack.conf.js
+++ test-config/wdio.browserstack.conf.js
@@ -5,7 +5,7 @@ const commonBStackCapabilities = {
   video: true,
   debug: true,
   networkLogs: true,
-  consoleLogs: 'error'
+  consoleLogs: 'errors'
 }
 
 // https://www.browserstack.com/automate/capabilities
