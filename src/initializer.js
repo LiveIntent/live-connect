012db12ec31105a8f2dc944c392317daa1bf590e
wiem <welabidine@liveintent.com>
2022-11-15 14:54:58 +0100

    small refactoring

--- src/initializer.js
+++ src/initializer.js
@@ -4,8 +4,6 @@ import { isObject } from './utils/types'
 import { EVENT_BUS_NAMESPACE } from './utils/consts'
 import { GlobalEventBus } from './events/event-bus'
 
-let _minimalMode
-
 /**
 * @param {LiveConnectConfiguration} liveConnectConfig
 * @param {StorageHandler} externalStorageHandler
@@ -17,9 +15,9 @@ let _minimalMode
 */
 export function LiveConnect (liveConnectConfig, externalStorageHandler, externalCallHandler, mode, externalEventBus) {
   console.log('Initializing LiveConnect', liveConnectConfig)
-  _minimalMode = mode === 'minimal' || process.env.LiveConnectMode === 'minimal'
+  const minimalMode = mode === 'minimal' || process.env.LiveConnectMode === 'minimal'
   const bus = externalEventBus || GlobalEventBus(EVENT_BUS_NAMESPACE, 5, err => console.error(err))
   const configuration = (isObject(liveConnectConfig) && liveConnectConfig) || {}
-  const _initializationFunction = _minimalMode ? MinimalLiveConnect : StandardLiveConnect
+  const _initializationFunction = minimalMode ? MinimalLiveConnect : StandardLiveConnect
   return _initializationFunction(configuration, externalStorageHandler, externalCallHandler, bus)
 }
