388666342e2820d06952f7b20d2777e84e388c42
Leonel Cuevas Valeriano <16063629+leonelcuevas@users.noreply.github.com>
2022-11-16 10:48:54 +0100

    Update src/config-validators/remove-invalid-pairs.js
    Co-authored-by: Wiem Zine El Abidine <welabidine@liveintent.com>

--- src/config-validators/remove-invalid-pairs.js
+++ src/config-validators/remove-invalid-pairs.js
@@ -3,7 +3,7 @@
  * @return {LiveConnectConfiguration}
  */
 export function removeInvalidPairs (config, eventBus) {
-  if (config.appId && config.distributorId) {
+  if (config && config.appId && config.distributorId) {
     const distributorId = config.distributorId
     delete config.distributorId
     eventBus.emitError('AppIdAndDistributorIdPresent', new Error(`Event contains both appId: ${config.appId} and distributorId: ${distributorId}. Ignoring distributorId`))
