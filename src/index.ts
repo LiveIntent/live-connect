9c59747533769d2fb97cf7bcbf268a73379275a8
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2023-01-09 14:54:27 +0100

    comments

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ export * from './initializer'
 export * from './minimal-live-connect'
 export * from './standard-live-connect'
 
-export * as emitter from './utils/emitter'
-export * as eventBus from './events/bus'
+export * as eventBus from './events/event-bus'
 export * as types from './utils/types'
