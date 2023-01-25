bfcffb59ee3111e0f9d47c6e4b564b0b3ceed0ad
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2023-01-26 00:21:41 +0100

    linter

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { StorageStrategy } from "./model/storage-strategy"
+import { StorageStrategy } from './model/storage-strategy'
 
 export interface ErrorDetails extends Error {
     stackTrace?: string,
