8047b4d14238174b862b9e1d4f987127b447b42c
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2023-02-27 18:09:13 +0100

    backwards compat

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,4 +1,8 @@
+import { ERRORS_CHANNEL } from "live-connect-common"
+
 export const EVENT_BUS_NAMESPACE = '__li__evt_bus'
+// reexport for backwards compat
+export const ERRORS_PREFIX = ERRORS_CHANNEL
 export const PIXEL_SENT_PREFIX = 'lips'
 export const PRELOAD_PIXEL = 'pre_lips'
 
