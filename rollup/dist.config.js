9fe607bc258f4534d8976bd144d6b8f234844266
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2023-09-08 17:02:49 +0200

    [CM-942] Update build config

--- rollup/dist.config.js
+++ rollup/dist.config.js
@@ -1,32 +1,39 @@
-import cleaner from 'rollup-plugin-cleaner'
-import ts from '@rollup/plugin-typescript'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
 import strip from '@rollup/plugin-strip'
-import babel from '@rollup/plugin-babel'
-import terser from '@rollup/plugin-terser'
+import ts from '@rollup/plugin-typescript'
+import cleaner from 'rollup-plugin-cleaner'
 import mjsEntry from 'rollup-plugin-mjs-entry'
+import dts from 'rollup-plugin-dts'
+import del from "rollup-plugin-delete";
+import terser from '@rollup/plugin-terser'
 
 const OUTPUT_DIR = './dist'
 
-export default {
-  input: './src/index.ts',
-  output: [
+export default [
     {
-      file: `${OUTPUT_DIR}/index.cjs`,
-      format: 'cjs',
-      sourcemap: true
+        input: './src/index.ts',
+        output: [
+            {
+                file: `${OUTPUT_DIR}/index.js`,
+                format: 'cjs',
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            cleaner({ targets: [OUTPUT_DIR] }),
+            ts({
+                compilerOptions: {
+                    outDir: OUTPUT_DIR,
+                    declarationDir: `${OUTPUT_DIR}/dts`,
+                }
+            }),
+            strip(),
+            terser(),
+            mjsEntry() // https://nodejs.org/api/packages.html#packages_dual_commonjs_es_module_packages
+        ]
+    },
+    {
+        input: `${OUTPUT_DIR}/dts/src/index.d.ts`,
+        output: [{ file: `${OUTPUT_DIR}/index.d.ts`, format: 'es' }],
+        plugins: [dts(), del({ targets: `${OUTPUT_DIR}/dts`, hook: 'buildEnd' })],
     }
-  ],
-  external: [/@babel\/runtime/],
-  plugins: [
-    cleaner({ targets: [OUTPUT_DIR] }),
-    ts(),
-    resolve({ preferBuiltins: true }),
-    commonjs({ defaultIsModuleExports: true }),
-    strip(),
-    babel({ babelHelpers: 'runtime' }),
-    terser(),
-    mjsEntry() // https://nodejs.org/api/packages.html#packages_dual_commonjs_es_module_packages
-  ]
-}
+]
