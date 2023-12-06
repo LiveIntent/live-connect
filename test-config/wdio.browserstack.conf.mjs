9893d3a8f351704e3d2c96093795fefa04f1b60c
3link <34981284+3link@users.noreply.github.com>
2023-12-06 09:52:20 +0100

    Chrome 47

--- test-config/wdio.browserstack.conf.mjs
+++ test-config/wdio.browserstack.conf.mjs
@@ -13,7 +13,7 @@ const commonBStackCapabilities = {
 // https://www.browserstack.com/automate/capabilities
 const allCapabilities = [
   { browserName: 'Chrome', browserVersion: 'latest', 'bstack:options': { ...commonBStackCapabilities, os: 'Windows', osVersion: '10' } },
-  { browserName: 'Chrome', browserVersion: '46.0', 'bstack:options': { ...commonBStackCapabilities, os: 'Windows', osVersion: '7' } },
+  { browserName: 'Chrome', browserVersion: '47.0', 'bstack:options': { ...commonBStackCapabilities, os: 'Windows', osVersion: '7' } },
   { browserName: 'firefox', browserVersion: 'latest', 'bstack:options': { ...commonBStackCapabilities, os: 'Windows', osVersion: '10' } },
   { browserName: 'firefox', browserVersion: '78.0', 'bstack:options': { ...commonBStackCapabilities, os: 'Windows', osVersion: '7' } },
   { browserName: 'Edge', browserVersion: 'latest', 'bstack:options': { ...commonBStackCapabilities, os: 'Windows', osVersion: '10' } },
