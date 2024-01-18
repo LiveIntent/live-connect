c4e749a779ed734b41c881e3f364e6a60a6567cb
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2024-01-18 13:43:12 +0100

    [No Ticket] Less repetetive typing of public constructors

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,21 @@
-import { CallHandler, EventBus, ReadOnlyStorageHandler, StorageHandler } from 'live-connect-common'
 import { LiveConnect as _LiveConnect } from './initializer'
 import { MinimalLiveConnect as _MinimalLiveConnect } from './minimal-live-connect'
 import { StandardLiveConnect as _StandardLiveConnect } from './standard-live-connect'
-import { LiveConnectConfig, PublicLiveConnect } from './types'
+import { PublicLiveConnect, LiveConnectConfig } from './types'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Returning<F extends (...args: any) => any, T> = (...args: Parameters<F>) => T
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type PublicConstructor<F extends (...args: any) => any> = Returning<F, PublicLiveConnect>
 
 // upcasted versions of the internal APIs
-export const LiveConnect: (
-  liveConnectConfig: LiveConnectConfig,
-  externalStorageHandler: StorageHandler,
-  externalCallHandler: CallHandler,
-  mode: 'minimal' | string,
-  externalEventBus?: EventBus
-) => PublicLiveConnect | null = _LiveConnect
+export const LiveConnect: PublicConstructor<typeof _LiveConnect> = _LiveConnect
 
-export const StandardLiveConnect: (
-  liveConnectConfig: LiveConnectConfig,
-  externalStorageHandler: StorageHandler,
-  externalCallHandler: CallHandler,
-  externalEventBus?: EventBus
-) => PublicLiveConnect = _StandardLiveConnect
+export const StandardLiveConnect: PublicConstructor<typeof _StandardLiveConnect> = _StandardLiveConnect
 
-export const MinimalLiveConnect: (
-  liveConnectConfig: LiveConnectConfig,
-  externalStorageHandler: ReadOnlyStorageHandler,
-  externalCallHandler: CallHandler,
-  externalEventBus?: EventBus,
-) => PublicLiveConnect = _MinimalLiveConnect
+export const MinimalLiveConnect: PublicConstructor<typeof _MinimalLiveConnect> = _MinimalLiveConnect
 
 export * as eventBus from './events/event-bus'
 export * as consts from './utils/consts'
 
-export { PublicLiveConnect, LiveConnectConfig } from './types'
+export { PublicLiveConnect, LiveConnectConfig }
--- src/initializer.ts
+++ src/initializer.ts
@@ -4,7 +4,7 @@ import { CallHandler, StorageHandler, isObject } from 'live-connect-common'
 import { EventBus, InternalLiveConnect, LiveConnectConfig } from './types'
 import { LocalEventBus } from './events/event-bus'
 
-export function LiveConnect(liveConnectConfig: LiveConnectConfig, externalStorageHandler: StorageHandler, externalCallHandler: CallHandler, mode: 'minimal' | string, externalEventBus?: EventBus): InternalLiveConnect | null {
+export function LiveConnect(liveConnectConfig: LiveConnectConfig, externalStorageHandler: StorageHandler, externalCallHandler: CallHandler, mode: 'minimal' | string, externalEventBus?: EventBus): InternalLiveConnect {
   const minimalMode = mode === 'minimal'
   const bus = externalEventBus || LocalEventBus()
   const configuration = (isObject(liveConnectConfig) && liveConnectConfig) || {}
