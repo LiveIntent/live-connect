38c937bac947ef20b096f90b08cf40fa078e26c2
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2024-01-22 15:31:04 +0100

    [No Ticket] Expose params

--- src/internal.ts
+++ src/internal.ts
@@ -3,6 +3,8 @@ export * from './initializer'
 export * from './minimal-live-connect'
 export * from './standard-live-connect'
 
+export * as params from './utils/params'
+
 export { DurableCache, StorageHandlerBackedCache } from './cache'
 export { WrappedStorageHandler } from './handlers/storage-handler'
 export { InternalLiveConnect, LiveConnectConfig, State } from './types'
